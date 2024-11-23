fe9e8457f74b7612ade6d9d781aa40fe3e379431
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-11-23 18:45:54 -0500

    develop header component and show component according to route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { BrowserRouter, Route, Router } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
 import Itinerary from "./pages/Itinerary/Itinerary";
@@ -8,11 +8,13 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import TripDetails from "./pages/TripDetails/TripDetails";
 import TripSetup from "./pages/TripSetup/TripSetup";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
         <BrowserRouter>
-            <Router>
+            <Header />
+            <Routes>
                 <Route path="/" element={Home} />
                 <Route path="/register" element={Register} />
                 <Route path="/login" element={Login} />
@@ -21,7 +23,7 @@ function App() {
                 <Route path="/trip/:tripId" element={TripDetails} />
                 <Route path="/itinerary/:tripId" element={Itinerary} />
                 <Route path="/list/:tripId" element={List} />
-            </Router>
+            </Routes>
         </BrowserRouter>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,39 @@
 import "./Header.scss";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import logoFull from "../../assets/logos/triply-logo-full-light.svg";
+import logoIcon from "../../assets/logos/triply-logo-icon-light.svg";
+import logoText from "../../assets/logos/triply-logo-text-light.svg";
+import avatar from "../../assets/images/avatar.png";
 
 function Header() {
-    return <div>Header</div>;
+    const location = useLocation();
+    const [route, setRoute] = useState(location.pathname);
+
+    useEffect(() => {
+        setRoute(location.pathname);
+    }, [location]);
+
+    return (
+        <>
+            {route === "/" && <></>}
+            {(route === "/register" || route === "/login") && (
+                <header className="header header--purple">
+                    <img className="header__logo-full" src={logoFull} alt="triply logo" />
+                </header>
+            )}
+            {route !== "/" && route !== "/register" && route !== "/login" && (
+                <header className="header header--grey">
+                    <Link className="header__logo-container" to="/dashboard">
+                        <img className="header__logo-icon" src={logoIcon} alt="triply logo icon" />
+                    </Link>
+                    <div className="header__avatar-container">
+                        <img className="header__avatar" src={avatar} alt="avatar" />
+                    </div>
+                </header>
+            )}
+        </>
+    );
 }
 
 export default Header;
