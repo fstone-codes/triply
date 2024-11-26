b6d34aa14bbdc0a39013a8312b406cbf0000880f
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-11-25 21:21:22 -0500

    add list page content, add axios request to fetch all lists, add axios reguest for trip details page

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,7 +1,40 @@
 import "./List.scss";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { baseUrl } from "../../utils/utils";
+import axios from "axios";
 
 function List() {
-    return <div>List</div>;
+    const [lists, setLists] = useState(null);
+    const { tripId } = useParams();
+
+    const getLists = async () => {
+        try {
+            const { data } = await axios.get(`${baseUrl}/api/lists`);
+
+            setLists(data);
+        } catch (error) {
+            console.error("Error fetching lists:", error);
+        }
+    };
+
+    useEffect(() => {
+        getLists();
+    }, []);
+
+    if (!lists) {
+        return <div>Loading lists...</div>;
+    }
+
+    return (
+        <ul>
+            {lists.map((list) => (
+                <Link key={list.id} to={`/trip/${tripId}/list/${list.id}`}>
+                    <li>{list.list_name}</li>
+                </Link>
+            ))}
+        </ul>
+    );
 }
 
 export default List;
--- src/pages/TripDetails/TripDetails.jsx
+++ src/pages/TripDetails/TripDetails.jsx
@@ -1,6 +1,31 @@
 import "./TripDetails.scss";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { baseUrl } from "../../utils/utils";
+import axios from "axios";
 
 function TripDetails() {
+    const [trip, setTrip] = useState(null);
+    const { tripId } = useParams();
+
+    const getTrip = async () => {
+        try {
+            const { data } = await axios.get(`${baseUrl}/api/trips/${tripId}`);
+
+            setTrip(data);
+        } catch (error) {
+            console.error("Error fetching trip:", error);
+        }
+    };
+
+    useEffect(() => {
+        getTrip();
+    }, [tripId]);
+
+    if (!trip) {
+        return <div>Loading trip...</div>;
+    }
+
     return <div>Trip Details</div>;
 }
 
