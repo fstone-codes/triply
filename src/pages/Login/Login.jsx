56b75625368390b62f1955b5c948b83b4c898a0a
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-11-27 18:11:36 -0500

    add elements and links to login and registry pages, update home page links

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,75 @@
 import "./Login.scss";
+import { baseUrl } from "../../utils/utils";
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import Button from "../../components/Button/Button";
+import FormInput from "../../components/FormInput/FormInput";
 
 function Login() {
-    return <div>Login</div>;
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData(() => ({
+            ...formData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        navigate(`/dashboard`);
+    };
+
+    // validation
+    const validateForm = () => {
+        if (!formData.email || !formData.password) {
+            console.error("Missing required fields");
+            return false;
+        }
+
+        return true;
+    };
+    return (
+        <main className="login">
+            <form className="login__form" onSubmit={handleSubmit}>
+                <h1 className="login__title">Login</h1>
+
+                <FormInput
+                    id="email"
+                    label="Email"
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    placeholder="Type your email"
+                    handleInputChange={handleInputChange}
+                />
+                <FormInput
+                    id="password"
+                    label="Password"
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    placeholder="Type your password"
+                    handleInputChange={handleInputChange}
+                />
+
+                <div className="login__button-container">
+                    <Button classType="primary" type="submit" text="Submit" />
+                    <Link className="login__link" to="/">
+                        <Button classType="secondary" type="button" text="Cancel" />
+                    </Link>
+                </div>
+            </form>
+        </main>
+    );
 }
 
 export default Login;
